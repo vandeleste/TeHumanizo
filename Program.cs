3506fc03527a7808c6a54a43fe3d2f9abffff96c
vandeleste <114308984+vandeleste@users.noreply.github.com>
2026-06-28 12:06:39 -0300

    .cs y csproj

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 class Program{
-    static void main(string[]dirTemp){
+    static void Main(string[]dirTemp){
         //data_Utau[]
     }
     static void Log(string text){
-        path_log=AppContext.BaseDirectory+"log_plugin.txt";;
+        string path_log=AppContext.BaseDirectory+"log_plugin.txt";
         File.AppendAllText(path_log,text+"\n");
     }
 }
